a67e4c81fc1a7578ec35120b89d37c86d77f8f8f
Gerald Bryant <hakeem1106@gmail.com>
2018-10-08 12:46:53 +0000

    fixed

--- main.js
+++ main.js
@@ -8,11 +8,7 @@
 
     app.controller("myControl", function($scope){
         $scope.madeAgain={
-<<<<<<< HEAD
            "background-image": "url('./images/PortBack.jpg')",
-=======
-           "background-image": "url('./images/PortBack.jpeg')",
->>>>>>> 6cc37b57c325fae4bc943b6c536f5337939543b9
             "resize": "both",
         "background-position": "center",
            "background-attachment": "fixed",
