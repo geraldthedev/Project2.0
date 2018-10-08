bcc72fd941582323b362b1ce3ee401a6ed653e39
Gerald Bryant <hakeem1106@gmail.com>
2018-10-08 11:27:59 +0000

    scope

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    
+
     var app = angular.module("myApp", []);
 
     app.controller("myControl", function($scope){
-        $Scope.madeAgain={
+        $scope.madeAgain={
            "background-color" : "blue"
         }
 
